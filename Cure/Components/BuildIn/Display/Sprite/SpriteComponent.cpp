d86009b516ab27ee91a8e87e2b6eaef7815c81df
majorek31 <76913151+majorek31@users.noreply.github.com>
2023-11-20 07:25:23 +0100

    chore: correct misspell

--- Cure/Components/BuildIn/Display/Sprite/SpriteComponent.cpp
+++ Cure/Components/BuildIn/Display/Sprite/SpriteComponent.cpp
@@ -5,7 +5,7 @@ namespace Cure {
 	void SpriteComponent::Start()
 	{
 		m_ObjectTransform = GetOwner()->GetComponent<TransformComponent>();
-		CURE_ASSERT(m_ObjectTransform, "Object has no TransfromComponent");
+		CURE_ASSERT(m_ObjectTransform, "Object has no TransformComponent");
 	}
 	void SpriteComponent::Update()
 	{
