ce218c09801a8f911814dac69d691ee90e71b248
majorek31 <m4y0r.offical@gmail.com>
2023-12-04 18:42:50 +0100

    feat: implement AudioAsset

--- Cure/Assets/Audio/AudioAsset.cpp
+++ Cure/Assets/Audio/AudioAsset.cpp
@@ -1,4 +1,34 @@
 #include "AudioAsset.h"
+
 namespace Cure {
+	AudioAsset::AudioAsset(const std::string& path, AudioType type) {
+		switch (type)
+		{
+		case Cure::AudioType::MP3:
+			CURE_UNIMPLEMENTED();
+			break;
+		case Cure::AudioType::WAV:
+			m_NativeAsset = Mix_LoadMUS(path.c_str());
+			break;
+		default:
+			break;
+		}
+		CURE_ASSERT(m_NativeAsset, "Couldn't load music asset");
+	}
+
+	AudioAsset::~AudioAsset()
+	{
+		Mix_FreeMusic(GetNativeMusic());
+	}
+
+	size_t AudioAsset::GetType()
+	{
+		return typeid(*this).hash_code();
+	}
+
+	Mix_Music* AudioAsset::GetNativeMusic()
+	{
+		return static_cast<Mix_Music*>(m_NativeAsset);
+	}
 
 }
--- Cure/Assets/Audio/AudioAsset.h
+++ Cure/Assets/Audio/AudioAsset.h
@@ -1,9 +1,15 @@
-#pragma once
+﻿#pragma once
+#include <SDL.h>
+#include <SDL_mixer.h>
 #include "../Asset.h"
+#include "AudioFiles.h"
 namespace Cure {
 	class AudioAsset : public Asset
 	{
-
+		AudioAsset(const std::string& path, AudioType type);
+		~AudioAsset();
+		size_t GetType() override;
+		Mix_Music* GetNativeMusic();
 	};
 }
 
