5b1226038ba3de2f620702f58bf4f0313874119b
majorek31 <m4y0r.offical@gmail.com>
2023-12-11 19:51:34 +0100

    feat: add singleton to audioplayer

--- Cure/AudioPlayer/AudioPlayer.cpp
+++ Cure/AudioPlayer/AudioPlayer.cpp
@@ -1,4 +1,11 @@
 #include "AudioPlayer.h"
 namespace Cure {
+	Cure::AudioPlayer* AudioPlayer::s_Instance = nullptr;
+	AudioPlayer& AudioPlayer::Get()
+	{
+		if (!s_Instance)
+			s_Instance = new AudioPlayer;
+		return *s_Instance;
+	}
 
 }
--- Cure/AudioPlayer/AudioPlayer.h
+++ Cure/AudioPlayer/AudioPlayer.h
@@ -4,9 +4,12 @@ namespace Cure {
 	class CURE_API AudioPlayer
 	{
 	public:
-
+		AudioPlayer& Get();
 	private:
-
+		AudioPlayer();
+		AudioPlayer(const AudioPlayer&) = delete;
+		AudioPlayer(AudioPlayer&) = delete;
+		static AudioPlayer* s_Instance;
 	};
 }
 
